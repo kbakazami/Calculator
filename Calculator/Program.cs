2ffc882a8a0a18485d2560a9cdbac51fdf37cf80
kbakazami <struys.wyvin@gmail.com>
2025-07-20 21:53:34 +0200

    Ajoutez des fichiers projet.

---
+++ Calculator/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace Calculator;
+class Calculator
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the Calculator!");
+        int firstNumber = AskNumber();
+        int secondNumber = AskNumber();
+        Console.WriteLine("Please enter an operator (+, -, *, /):");
+
+        string operatorSigne = Console.ReadLine();
+
+        switch (operatorSigne)
+        {   
+            case "+":
+                Addition(firstNumber, secondNumber); break;
+            case "-":
+                Soustraction(firstNumber, secondNumber); break;
+            case "*":
+                Multiplication(firstNumber, secondNumber); break;
+            case "/":
+                Division(firstNumber, secondNumber); break;
+        }
+    }
+
+    public static void Addition(int num01, int num02)
+    {
+        Console.WriteLine($"{num01} + {num02} = {num01 + num02}");
+    }
+
+    public static void Soustraction(int num01, int num02)
+    {
+        Console.WriteLine($"{num01} - {num02} = {num01 - num02}");
+    }
+
+    public static void Multiplication(int num01, int num02)
+    {
+        Console.WriteLine($"{num01} * {num02} = {num01 * num02}");
+    }
+
+    public static void Division(int num01, int num02)
+    {
+        try
+        {
+            Console.WriteLine($"{num01} / {num02} = {num01 / num02}");
+        }
+        catch(DivideByZeroException)
+        {
+            Console.WriteLine("Division par 0 détecté ; retour de 0 par défaut");
+        }
+    }
+
+    public static int AskNumber()
+    {
+        while (true)
+        {
+            try
+            {
+                Console.WriteLine("Please enter a number:");
+                return Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Not a number... Try again ...");
+            }
+        }
+    }
+}
